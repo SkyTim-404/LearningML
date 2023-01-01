5e9fce35f63020a7e2ed6463edd73af147c80ed7
SkyTim-404 <sls22142@gmail.com>
2023-01-01 20:44:05 +0800

    Add discriminator model

---
+++ image-generation/model/discriminator.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from model.module import ConvBlock, ResidualConvBlock, LinearBlock
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.convs = nn.ModuleList([
+            ConvBlock(in_channels=1, out_channels=16, kernel_size=3, padding=1), 
+            ResidualConvBlock(in_channels=16, num_layers=2, kernel_size=3, padding=1), 
+            ConvBlock(in_channels=16, out_channels=16, kernel_size=3, padding=1, stride=2), 
+            ConvBlock(in_channels=16, out_channels=32, kernel_size=3, padding=1), 
+            ResidualConvBlock(in_channels=32, num_layers=2, kernel_size=3, padding=1),
+            ConvBlock(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=2),
+            ConvBlock(in_channels=32, out_channels=64, kernel_size=3, padding=1, stride=1),
+            ResidualConvBlock(in_channels=64, num_layers=2, kernel_size=3, padding=1),
+            nn.AdaptiveAvgPool2d((1, 1)), 
+        ])
+        self.linears = nn.ModuleList([
+            LinearBlock(in_features=64, out_features=16),
+            LinearBlock(in_features=16, out_features=1)
+        ])
+        self.last_activation = nn.Sigmoid()
+        
+    
+    def forward(self, x):
+        for module in self.convs:
+            x = module(x)
+        x = torch.squeeze(x)
+        for module in self.linears:
+            x = module(x)
+        x = self.last_activation(x)
+        return x
---
+++ image-generation/model/module.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=0, stride=1):
+        super().__init__()
+        self.batch_norm = nn.BatchNorm2d(in_channels)
+        self.leaky_relu = nn.LeakyReLU(0.01)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding)
+    
+    def forward(self, x):
+        return self.conv(self.leaky_relu(self.batch_norm(x)))
+
+
+class ConvTransposeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        
+    def forward(self, x):
+        pass
+    
+    
+class ResidualConvBlock(nn.Module):
+    def __init__(self, in_channels, num_layers=2, kernel_size=3, padding=1):
+        super().__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(num_layers):
+            self.convs.append(nn.BatchNorm2d(in_channels))
+            self.convs.append(nn.LeakyReLU(0.01))
+            self.convs.append(nn.Conv2d(in_channels, in_channels, kernel_size, padding=padding))
+    
+    def forward(self, x):
+        identity = torch.clone(x)
+        for module in self.convs:
+            x = module(x)
+        return identity + x
+    
+    
+class LinearBlock(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.batch_norm = nn.BatchNorm1d(in_features)
+        self.leaky_relu = nn.LeakyReLU(0.01)
+        self.dropout = nn.Dropout(0.5)
+        self.linear = nn.Linear(in_features, out_features)
+    
+    def forward(self, x):
+        return self.linear(self.dropout(self.leaky_relu(self.batch_norm(x))))
