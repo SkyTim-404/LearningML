d000a3976396493cb6e1f64699aa696981550545
SkyTim-404 <sls22142@gmail.com>
2023-01-07 19:02:19 +0800

    Add wgan loss

--- image-generation/loss.py
+++ image-generation/loss.py
@@ -15,3 +15,19 @@ class GeneratorLoss(nn.Module):
         
     def forward(self, predict_fake):
         return -torch.mean(torch.log(predict_fake+1e-8))
+    
+    
+class WassersteinCriticLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, predict_real, predict_fake):
+        return -(torch.mean(predict_real) - torch.mean(predict_fake))
+
+
+class WassersteinGeneratorLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, predict_fake):
+        return -torch.mean(predict_fake)
